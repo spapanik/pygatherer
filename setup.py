290a690ff5a3fcc32b67773a60cefcdb58b9440e
Steph Papanik <spapanik21@gmail.com>
2018-08-29 18:20:13 +0100

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 from setuptools import find_packages, setup
 
+__author__ = "spapanik"
+__license__ = "MIT"
+__version__ = "0.0.1"
+
+
+PKG_NAME = "pygatherer"
+PKG_URL = f"https://github.com/{__author__}/{PKG_NAME}"
+
 setup(
-    name="pygatherer",
-    license="MIT",
+    name=PKG_NAME,
+    version=__version__,
+    license=__license__,
+    description="An API for the gatherer",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    install_requires=[
+        "requests>=2.0,<3.0" "beautifulsoup4>=4.0,<5.0" "lxml>=4.0,<5.0"
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
 )
