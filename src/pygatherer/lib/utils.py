4dcaabad1b82766a8265bf1eff0cad387f5e7c2d
Stephanos Kuma <stephanos@kuma.ai>
2025-03-04 14:49:07 +0000

    ⏫ Update dependencies

--- src/pygatherer/lib/utils.py
+++ src/pygatherer/lib/utils.py
@@ -1,15 +1,14 @@
 from enum import Enum
 from functools import lru_cache
-from typing import Any
 
 
 class BaseEnum(Enum):
     @staticmethod
-    def _generate_next_value_(  # type: ignore[misc]
+    def _generate_next_value_(
         name: str,
         start: int,  # noqa: ARG004
         count: int,  # noqa: ARG004
-        last_values: list[Any],  # noqa: ARG004
+        last_values: list[str],  # noqa: ARG004
     ) -> str:
         return name.replace("_", " ").title()
 
